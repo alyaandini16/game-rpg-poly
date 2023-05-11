5acb7a60a546aafd8bbd6aee3c23ff3f9a96a37e
alyaandini16 <133179799+alyaandini16@users.noreply.github.com>
2023-05-11 08:54:12 +0700

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -2,7 +2,6 @@ public class Hero extends Character {
     public void move(){
     System.out.println("Hero John melompat");
     }
-
     public void move(String direction){
     System.out.println("Hero John melompat ke "+direction);
     }
